72b0dfbb6306fa98729097396d589bac3ef1802a
Laura902l <121919112+Laura902l@users.noreply.github.com>
2024-01-28 12:32:56 +0600

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(express.static('public'));
 
 const travelRoutes = require('./routes/travelRoutes');
 const staticRoutes = require('./routes/static'); // Corrected the require statement
-const tourHistoryRoutes = require('./routes/tourHistoryRoutes'); // Add this line
-app.use('/tourHistory', tourHistoryRoutes); // Add this line
 
 
 app.use('/travel', travelRoutes);
